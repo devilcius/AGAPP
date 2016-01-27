d171d579b1783b7f717140de5605eca8c3431a4c
Marcos <devilcius@gmail.com>
2016-01-27 11:33:38 +0100

    First version with comunicados

---
+++ www/js/services.js
@@ -0,0 +1,10 @@
+angular.module('starter.services', ['ngResource'])
+
+.factory('Post', function ($resource) {
+    return $resource(
+    'http://aga.com/app_dev.php/api/news/posts/:postId' + '.json',
+    { page: 1, enabled: 1 },
+    {'query': { method: 'GET', isArray: false }}
+    );
+});
+
